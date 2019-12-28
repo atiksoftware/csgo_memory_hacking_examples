8386811de63844533401213757cd8873b947d37a
Atiksoftware Corporation <atiksoftware@gmail.com>
2019-12-28 07:29:59 +0300

    Create Get_C4_Is_Planted.cpp

---
+++ examples/Get_C4_Is_Planted.cpp
@@ -0,0 +1,7 @@
+/*
+If you find a entity class id's as 128 in list after players(128 index), its mean C4 is planted
+  
+Eğer entity list içinde playerlardan sonraki elemanlar içinde bir entity nin class id si 128 olan bir item bulursanız, 
+bunun anlamı c4 kuruldu demektir. 
+ 
+ */
